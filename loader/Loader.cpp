d1255e72bdb6152e8afe94211eae2e3bc3281763
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-02-10 14:12:42 +0800

    Update Loader.cpp

--- loader/Loader.cpp
+++ loader/Loader.cpp
@@ -689,8 +689,6 @@ void vendorBpfLoader() {
         ALOGE("If this triggers randomly, you might be hitting some memory allocation "
               "problems or startup script race.");
         ALOGE("--- DO NOT EXPECT SYSTEM TO BOOT SUCCESSFULLY ---");
-        sleep(20);
-        exit(121);
     }
 
     const char* args[] = {"/apex/com.android.tethering/bin/netbpfload", "done", NULL};
